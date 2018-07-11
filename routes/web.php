86ca7b268e99029db2dcc6a0efd746734056dd30
jngdwk <w7215809@163.com>
2018-07-11 16:46:46 +0800

    异常处理

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::group(['middleware' => 'email_verified'], function() {
 
-        });
     });
 
 });
